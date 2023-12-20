de414ad5a82de09ce52c6dd2fdd72cdf507073c8
Jesse Bakker <jesse.bakker@kadaster.nl>
2023-12-20 10:19:58 +0100

    Update terms/defs and add pathmapping execution

--- Samenvatting.md
+++ Samenvatting.md
@@ -1,6 +1,3 @@
 ## Abstract
 
-Informationmodel - application schema of lineage information as it is developed and applied in the WaU project 'Coherent semantic model and API orchestration for integrated data retrieval over decentralized API's'
-
-
-
+This document specifies the IMX Lineage model. The IMX lineage model provenance information generated during an IMX orchestration process, building upon [[IMX-Metadata]] and [[IMX-ModelMapping]].
--- js/config.js
+++ js/config.js
@@ -127,17 +127,17 @@ var respecConfig =
         publisher:  "Publisher-1",
         company:    "Interactive Instruments",
     },
-    "WAU-MAP":
+    "IMX-ModelMapping":
     {
-      title:      "WaU IMX Model Mapping",
-      href:       "https://geonovum.github.io/WaU-MAP",
+      title:      "IMX Model Mapping",
+      href:       "https://geonovum.github.io/IMX-ModelMapping",
       status:     "ED",
       publisher:  "Geonovum",
   },
-    "WAU-Metadata":
+    "IMX-Metadata":
       {
-        title:      "WaU - Metadata basic principles",
-        href:       "https://geonovum.github.io/WaU-Metadata/",
+        title:      "IMX - Metadata basic principles",
+        href:       "https://geonovum.github.io/IMX-Metadata",
         status:     "ED",
         publisher:  "Geonovum",
     },
