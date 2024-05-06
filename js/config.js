98dbc236f55fab4e8db7a6b0b2467fa3c10806ca
Arnoud de Boer <a.deboer@geonovum.nl>
2024-05-06 09:33:39 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,9 +4,9 @@ let respecConfig = {
   title: "IMX Lineage model",
   pluralize: true,
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  //specStatus: "wv",                   // Werkversie
+  specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
-  specStatus: "vv",               // Versie ter vaststelling
+  //specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
@@ -34,13 +34,13 @@ let respecConfig = {
 
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
- publishDate: "2024-05-01",
+ //publishDate: "2024-05-01",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
   //previousVersion: "0.0.1",
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
-  previousPublishDate: "2024-01-08",  	    // Format is "YYY-MM-DD"
-  previousMaturity: "CV",                   // kies 1 van deze 2 regels  	  
+  previousPublishDate: "2024-05-01",  	    // Format is "YYY-MM-DD"
+  previousMaturity: "VV",                   // kies 1 van deze 2 regels  	  
   //-- TODO: de namen van de Editor(s) / Redacteur(en)---------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors: 
